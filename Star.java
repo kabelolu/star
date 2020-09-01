59d05e8f5a3dafc37941babbd2d7bd93479666e4
Kabelo Lucas Kekana <kabelolu86@gmail.com>
2020-09-01 08:58:38 +0200

    Star Project

---
+++ Star.java
@@ -0,0 +1,13 @@
+
+
+class Star{
+Star(){}
+public static void main(String [] args){
+for (int a = 5; a>=1; a=a-1){
+for (int b = 0; b<a; b=b+1){
+System.out.print("*");
+}
+System.out.println();
+}
+}
+}
